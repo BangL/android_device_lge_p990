ce68d4f21fc0c73320d5215c3114efcacc7b59e4
BangL <henno.rickowski@googlemail.com>
2013-08-06 00:59:45 +0200

    reorg

--- p990.mk
+++ p990.mk
@@ -1,55 +1,78 @@
-# The gps config appropriate for this device
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/gps.conf:/system/etc/gps.conf
-
 $(call inherit-product, device/lge/star-common/star.mk)
-
-DEVICE_PACKAGE_OVERLAYS += device/lge/p990/overlay
 
 # Inherit non-open-source blobs.
 $(call inherit-product-if-exists, vendor/lge/p990/p990-vendor.mk)
 
-# Board-specific init
-PRODUCT_COPY_FILES += \
-    device/lge/p990/init.p990.rc:root/init.star.rc \
-    $(LOCAL_PATH)/ueventd.tegra.rc:root/ueventd.star.rc \
-    $(LOCAL_PATH)/fstab.p990:root/fstab.p990
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/charger:root/charger \
-    $(foreach f,$(wildcard $(LOCAL_PATH)/prebuilt/res/images/charger/*),$(f):root/res/images/charger/$(notdir $(f))) \
-    $(foreach f,$(wildcard $(LOCAL_PATH)/prebuilt/res/images/security/*),$(f):root/res/images/security/$(notdir $(f)))
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
-    $(LOCAL_PATH)/init.vsnet:system/bin/init.vsnet \
-    $(LOCAL_PATH)/init.vsnet-down:system/bin/init.vsnet-down \
-    $(LOCAL_PATH)/gps_brcm_conf.xml:system/etc/gps_brcm_conf.xml
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/check_sdcard.sh:system/bin/check_sdcard.sh \
-    $(LOCAL_PATH)/lgdrm.img:system/bin/lgdrm.img
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/kernel/zImage:kernel \
-    $(LOCAL_PATH)/kernel/bthid.ko:system/lib/modules/bthid.ko \
-    $(LOCAL_PATH)/kernel/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-    $(LOCAL_PATH)/kernel/wireless.ko:system/lib/modules/wireless.ko \
-    $(LOCAL_PATH)/kernel/zram.ko:system/lib/modules/zram.ko \
-    $(LOCAL_PATH)/kernel/alsa_amixer:system/xbin/alsa_amixer \
-    $(LOCAL_PATH)/kernel/95kowalski:system/etc/init.d/95kowalski \
-    $(LOCAL_PATH)/kernel/power.tegra.so:system/lib/hw/power.tegra.so \
-    $(LOCAL_PATH)/kernel/KowalskiManager.apk:system/app/KowalskiManager.apk
-
-PRODUCT_PACKAGES += \
-    lgcpversion
+### SETTINGS
 
 PRODUCT_NAME := full_p990
 PRODUCT_DEVICE := p990
 PRODUCT_MODEL := LG-P990
 
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Try to get "temasek version" from global var
 ifeq ($(TEMASEK_VERSION),)
-  TEMASEK_VERSION := Unkown-Private-Build
+  TEMASEK_VERSION := Unknown
 endif
 ADDITIONAL_BUILD_PROPERTIES += ro.temasek.version=$(TEMASEK_VERSION)
+
+
+### CONFIGS
+
+# The gps config appropriate for this device
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps_brcm_conf.xml:system/etc/gps_brcm_conf.xml
+
+# mount sd cards
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab
+
+# vsnet stuffs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init.vsnet:system/bin/init.vsnet \
+    $(LOCAL_PATH)/configs/init.vsnet-down:system/bin/init.vsnet-down
+
+
+### ROOTDIR
+
+# Board-specific init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/init.p990.rc:root/init.star.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.tegra.rc:root/ueventd.star.rc \
+    $(LOCAL_PATH)/rootdir/fstab.p990:root/fstab.p990
+
+# Decive off charger fix for new bl by pengus77
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/charger:root/charger \
+    $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/res/images/charger/*),$(f):root/res/images/charger/$(notdir $(f))) \
+    $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/res/images/security/*),$(f):root/res/images/security/$(notdir $(f)))
+
+
+### PREBUILT
+
+# lgdrm / sanity check stuff by pengus77
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/check_sdcard.sh:system/bin/check_sdcard.sh \
+    $(LOCAL_PATH)/prebuilt/lgdrm.img:system/bin/lgdrm.img
+
+# include prebuilt Kowalski kernel exp126 by pengus77
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/kernel/zImage:kernel \
+    $(LOCAL_PATH)/prebuilt/kernel/bthid.ko:system/lib/modules/bthid.ko \
+    $(LOCAL_PATH)/prebuilt/kernel/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
+    $(LOCAL_PATH)/prebuilt/kernel/wireless.ko:system/lib/modules/wireless.ko \
+    $(LOCAL_PATH)/prebuilt/kernel/zram.ko:system/lib/modules/zram.ko \
+    $(LOCAL_PATH)/prebuilt/kernel/alsa_amixer:system/xbin/alsa_amixer \
+    $(LOCAL_PATH)/prebuilt/kernel/95kowalski:system/etc/init.d/95kowalski \
+    $(LOCAL_PATH)/prebuilt/kernel/power.tegra.so:system/lib/hw/power.tegra.so \
+    $(LOCAL_PATH)/prebuilt/kernel/KowalskiManager.apk:system/app/KowalskiManager.apk
+
+
+### BUILD
+
+# bb version
+PRODUCT_PACKAGES += \
+    lgcpversion
