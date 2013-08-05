21828a7e8fa8e6f0293d77cb038e792c7d153f02
BangL <henno.rickowski@googlemail.com>
2013-08-05 02:56:04 +0200

    reorg

--- p990.mk
+++ p990.mk
@@ -1,30 +1,32 @@
-# The gps config appropriate for this device
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/gps.conf:/system/etc/gps.conf
-
 $(call inherit-product, device/lge/star-common/star.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/lge/p990/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Inherit non-open-source blobs.
-$(call inherit-product-if-exists, vendor/lge/p990/p990-vendor.mk)
+$(call inherit-product-if-exists, $(LOCAL_PATH)/p990-vendor.mk)
 
 # Board-specific init
 PRODUCT_COPY_FILES += \
-    device/lge/p990/init.p990.rc:root/init.star.rc \
-    $(LOCAL_PATH)/ueventd.tegra.rc:root/ueventd.star.rc \
-    $(LOCAL_PATH)/fstab.p990:root/fstab.p990
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/charger:root/charger \
-    $(foreach f,$(wildcard $(LOCAL_PATH)/prebuilt/res/images/charger/*),$(f):root/res/images/charger/$(notdir $(f))) \
-    $(foreach f,$(wildcard $(LOCAL_PATH)/prebuilt/res/images/security/*),$(f):root/res/images/security/$(notdir $(f)))
+    $(LOCAL_PATH)/rootdir/init.p990.rc:root/init.star.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.tegra.rc:root/ueventd.star.rc \
+    $(LOCAL_PATH)/rootdir/fstab.p990:root/fstab.p990
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.vsnet:system/bin/init.vsnet \
-    $(LOCAL_PATH)/init.vsnet-down:system/bin/init.vsnet-down \
-    $(LOCAL_PATH)/gps_brcm_conf.xml:system/etc/gps_brcm_conf.xml
+    $(LOCAL_PATH)/init.vsnet-down:system/bin/init.vsnet-down
 
+# Charger fix
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/charger:root/charger \
+    $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/res/images/charger/*),$(f):root/res/images/charger/$(notdir $(f))) \
+    $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/res/images/security/*),$(f):root/res/images/security/$(notdir $(f)))
+
+# The gps config appropriate for this device
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps_brcm_conf.xml:system/etc/gps_brcm_conf.xml
+
+# prebuilt kowalski kernel
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/kernel/zImage:kernel \
     $(LOCAL_PATH)/kernel/bthid.ko:system/lib/modules/bthid.ko \
