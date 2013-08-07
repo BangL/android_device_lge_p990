3b870aef92e088376af99c737c9d0064638d4d7a
BangL <henno.rickowski@googlemail.com>
2013-08-07 17:02:04 +0200

    import star-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,78 @@
 # inherit from the proprietary version
--include device/lge/star-common/BoardConfigCommon.mk
+-include vendor/lge/p990/BoardConfigVendor.mk
 
+# Misc
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := tegra
+TARGET_OTA_ASSERT_DEVICE := p990
 TARGET_BOOTLOADER_BOARD_NAME := p990
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+TARGET_CPU_VARIANT := tegra2
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_USE_NON_NEON_MEMCPY := true
+TARGET_SPECIFIC_HEADER_PATH := device/lge/p990/include
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/lge/p990/releasetools/p990-newbl_ota_from_target_files
+COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB -DICS_CAMERA_BLOB
 
+# Ril
+BOARD_RIL_CLASS := ../../../device/lge/p990/ril/
+
+# Kernel
 BOARD_KERNEL_CMDLINE := 
-
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_PAGE_SIZE := 0x00000800
+TARGET_PREBUILT_KERNEL := device/lge/p990/kernel/zImage
+#TARGET_KERNEL_SOURCE := kernel/lge/star
+#TARGET_KERNEL_CONFIG := cyanogenmod_p990_defconfig
 
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+
+# Camera
+BOARD_CAMERA_HAVE_ISO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_ISO
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+TARGET_NEEDS_BLUETOOTH_INIT_DELAY := true
+BOARD_BLUEDROID_VENDOR_CONF := device/lge/p990/configs/vnd_bt.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p990/bluetooth
+
+# Wlan
+BOARD_WLAN_DEVICE := bcm4329
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/wl/rtecdc.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/wl/rtecdc-apsta.bin"
+WIFI_DRIVER_MODULE_NAME := "wireless"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wireless.ko"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wl/rtecdc.bin nvram_path=/etc/wl/nvram.txt config_path=/data/misc/wifi/config"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_wext
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+WIFI_DRIVER_HAS_LGE_SOFTAP := true
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+BOARD_WEXT_NO_COMBO_SCAN := true
+
+# Graphics
+BOARD_USE_SKIA_LCDTEXT := true
+USE_OPENGL_RENDERER := true
+BOARD_NO_ALLOW_DEQUEUE_CURRENT_BUFFER := true
+BOARD_EGL_CFG := device/lge/p990/configs/egl.cfg
+BOARD_EGL_NEEDS_LEGACY_FB := true
+
+# Audio
+TARGET_OVERLAY_ALWAYS_DETERMINES_FORMAT := true
+TARGET_DONT_SET_AUDIO_AAC_FORMAT := true
+BOARD_USES_AUDIO_LEGACY := true
+
+# Partitions
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00800000
@@ -15,18 +80,24 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1610612736
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-#TARGET_KERNEL_SOURCE := kernel/lge/star
-#TARGET_KERNEL_CONFIG := cyanogenmod_p990_defconfig
-TARGET_PREBUILT_KERNEL := device/lge/p990/kernel/zImage
-
 BOARD_VOLD_MAX_PARTITIONS := 20
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
-BOARD_HAVE_FM_RADIO := true
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+# Buttons
+BOARD_HAS_NO_SELECT_BUTTON := true
 
-TARGET_OTA_ASSERT_DEVICE := p990
-BOARD_RIL_CLASS := ../../../device/lge/p990/ril/
+# Data
+BOARD_MOBILEDATA_INTERFACE_NAME := "vsnet0"
 
+# Recovery
+TARGET_RECOVERY_PRE_COMMAND := "/system/bin/setup-recovery"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/fsl-tegra-udc/gadget/lun%d/file"
 TARGET_RECOVERY_FSTAB := device/lge/p990/rootdir/fstab.p990
 RECOVERY_FSTAB_VERSION := 2
+
+# Vibrator
+BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/lge/p990/vibrator/vibrator.c
+
+# Light sensor
+BOARD_SYSFS_LIGHT_SENSOR := "/sys/class/backlight/aat2870-backlight/brightness_mode"
--- p990.mk
+++ p990.mk
@@ -1,50 +1,154 @@
-$(call inherit-product, device/lge/star-common/star.mk)
-
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
 # Inherit non-open-source blobs.
 $(call inherit-product-if-exists, $(LOCAL_PATH)/p990-vendor.mk)
+
+# Settings
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+$(call inherit-product, build/target/product/full_base_telephony.mk)
+PRODUCT_LOCALES += hdpi
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_NAME := full_p990
+PRODUCT_DEVICE := p990
+PRODUCT_MODEL := LG-P990
 
 # Board-specific init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.p990.rc:root/init.star.rc \
+    $(LOCAL_PATH)/rootdir/init.cm-star.rc:root/init.cm-star.rc \
+    $(LOCAL_PATH)/rootdir/init.star.usb.rc:root/init.star.usb.rc \
+    $(LOCAL_PATH)/rootdir/init_recovery.rc:root/init_recovery.rc \
     $(LOCAL_PATH)/rootdir/ueventd.tegra.rc:root/ueventd.star.rc \
     $(LOCAL_PATH)/rootdir/fstab.p990:root/fstab.p990
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/init.vsnet:system/bin/init.vsnet \
-    $(LOCAL_PATH)/configs/init.vsnet-down:system/bin/init.vsnet-down
-
 # Charger fix
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/charger:root/charger \
-    $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/res/images/charger/*),$(f):root/res/images/charger/$(notdir $(f))) \
-    $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/res/images/security/*),$(f):root/res/images/security/$(notdir $(f)))
+    $(LOCAL_PATH)/charger/charger:root/charger \
+    $(foreach f,$(wildcard $(LOCAL_PATH)/charger/res/images/charger/*),$(f):root/res/images/charger/$(notdir $(f))) \
+    $(foreach f,$(wildcard $(LOCAL_PATH)/charger/res/images/security/*),$(f):root/res/images/security/$(notdir $(f)))
 
-# The gps config appropriate for this device
+# Recovery
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
+
+# Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/asound.conf:system/etc/asound.conf \
+    $(LOCAL_PATH)/configs/egl.cfg:system/lib/egl/egl.cfg \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/configs/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    $(LOCAL_PATH)/configs/init.vsnet:system/bin/init.vsnet \
+    $(LOCAL_PATH)/configs/init.vsnet-down:system/bin/init.vsnet-down \
     $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps_brcm_conf.xml:system/etc/gps_brcm_conf.xml
 
+# Prebuilts
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/setup-recovery:system/bin/setup-recovery \
+    $(LOCAL_PATH)/prebuilt/star_synaptics.idc:system/usr/idc/star_synaptics.idc \
+    $(LOCAL_PATH)/prebuilt/star_synaptics.kl:system/usr/keylayout/star_synaptics.kl \
+    $(LOCAL_PATH)/prebuilt/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/prebuilt/tegra-snd-wm8994.kl:system/usr/keylayout/tegra-snd-wm8994.kl \
+    $(LOCAL_PATH)/prebuilt/usb_keyboard_102_en_us.kl:system/usr/keylayout/usb_keyboard_102_en_us.kl \
+    $(LOCAL_PATH)/prebuilt/usb_keyboard_102_en_us.kcm.bin:system/usr/keychars/usb_keyboard_102_en_us.kcm.bin \
     $(LOCAL_PATH)/prebuilt/check_sdcard.sh:system/bin/check_sdcard.sh \
     $(LOCAL_PATH)/prebuilt/lgdrm.img:system/bin/lgdrm.img
 
-# prebuilt kowalski kernel
+# prebuilt kowalski exp126 kernel
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/kernel/zImage:kernel \
-    $(LOCAL_PATH)/prebuilt/kernel/bthid.ko:system/lib/modules/bthid.ko \
-    $(LOCAL_PATH)/prebuilt/kernel/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-    $(LOCAL_PATH)/prebuilt/kernel/wireless.ko:system/lib/modules/wireless.ko \
-    $(LOCAL_PATH)/prebuilt/kernel/zram.ko:system/lib/modules/zram.ko \
-    $(LOCAL_PATH)/prebuilt/kernel/alsa_amixer:system/xbin/alsa_amixer \
-    $(LOCAL_PATH)/prebuilt/kernel/95kowalski:system/etc/init.d/95kowalski \
-    $(LOCAL_PATH)/prebuilt/kernel/power.tegra.so:system/lib/hw/power.tegra.so \
-    $(LOCAL_PATH)/prebuilt/kernel/KowalskiManager.apk:system/app/KowalskiManager.apk
+    $(LOCAL_PATH)/kernel/zImage:kernel \
+    $(LOCAL_PATH)/kernel/bthid.ko:system/lib/modules/bthid.ko \
+    $(LOCAL_PATH)/kernel/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
+    $(LOCAL_PATH)/kernel/wireless.ko:system/lib/modules/wireless.ko \
+    $(LOCAL_PATH)/kernel/zram.ko:system/lib/modules/zram.ko \
+    $(LOCAL_PATH)/kernel/alsa_amixer:system/xbin/alsa_amixer \
+    $(LOCAL_PATH)/kernel/95kowalski:system/etc/init.d/95kowalski \
+    $(LOCAL_PATH)/kernel/power.tegra.so:system/lib/hw/power.tegra.so \
+    $(LOCAL_PATH)/kernel/KowalskiManager.apk:system/app/KowalskiManager.apk
 
+# Permission files
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+
+# Packages
 PRODUCT_PACKAGES += \
+    bridgeutil \
+    lights.star \
+    libbridge \
+    libbridge_jni \
+    screencap \
+    audio.a2dp.default \
+    hwcomposer.default \
+    com.android.future.usb.accessory \
+    camerawrapper \
+    camera.tegra \
+    Torch \
     lgcpversion
 
-PRODUCT_NAME := full_p990
-PRODUCT_DEVICE := p990
-PRODUCT_MODEL := LG-P990
+# Ugly space-saving hack
+PRODUCT_PACKAGES += \
+    srec-en
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/10-movestuff.sh:system/addon.d/10-movestuff.sh
+
+# Force MTP/ADB to be availible on bootup
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.service.adb.enable=1 \
+    ro.adb.secure=0 \
+    persist.sys.usb.config=mtp,adb
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/20-mtp:system/etc/init.d/20-mtp
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=131072 \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.call_ring.delay=3000 \
+    ro.telephony.call_ring.absent=true \
+    ro.hardware.respect_als=true \
+    ro.bt.bdaddr_path=/sys/devices/platform/bd_address/bdaddr_if \
+    debug.sf.electron_frames=42 \
+    nv-camera-disable-early-graph=1 \
+    sys.mem.max_hidden_apps=4 \
+    ro.lge.audio_soundexception=true \
+    ro.bq.gpu_to_cpu_unsupported=1
+
+# Enable ZRAM by default
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.default=18 \
+    persist.service.zram=18
+
+# Dexopt settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-data-only=1 \
+    dalvik.vm.dexopt-flags=m=y,u=n,v=n,o=v
+
+# Disable sending usage data
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.nocheckin=1
+
+# Enable AGPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.def.agps.mode=1
+
+# Enable purgeable assets
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.purgeable_assets=1
+
+# Enable debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.debuggable=1
